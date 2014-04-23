f43b6c54b256305369a49d77c98dfccdd382076b
Sanghati Mukherjee <sanghatibm88@gmail.com>
2014-04-23 18:40:00 +0530

    Update business_hour.rb

--- business_hour.rb
+++ business_hour.rb
@@ -1,6 +1,6 @@
-require_relative './day'
 require 'date'
 require 'time'
+require 'pp'
 
 class BusinessHours
 
@@ -8,95 +8,86 @@ class BusinessHours
 	def initialize(default_opening_time,default_closing_time)
 		@default_opening_time = default_opening_time
 		@default_closing_time = default_closing_time
-		@days = ["Mon","Tue","Wed","Thur","Fri","Sat","Sun"]
-		@todays_date = Date.today
-		@calendar = []
+		@days = [:mon, :tue, :wed, :thu, :fri, :sat, :sun]
+		@update_days = {}
+		@closed_days = {}
 		@deadline = nil
+		@time_left = nil
 	end
 
-	def auto_generate_calendar
-		@days.each do |day|
-			calendar_day = Day.new
-			calendar_day.date = date_of_next(day)
-			calendar_day.name_of_day = day
-			calendar_day.day_opening_time = @default_opening_time
-			calendar_day.day_closing_time = @default_closing_time
-			calendar_day.is_opened = "opened"
-			@calendar.push(calendar_day)
-		end
+	def update(update_day_date, opening_time, closing_time)
+		
+		day = update_day_date.is_a?(Symbol) ? update_day_date : Date.parse(update_day_date) 	
+		
+		@update_days[day] = [opening_time, closing_time]
 	end
 
-	def update(update_day_date,update_opening_time, update_closing_time)
-		@calendar.each do |cal|
-			if update_day_date.match('[A-z]{3}\s\d{2}[,]\s\d{4}')
-				if(cal.date == Date.parse(update_day_date))
-					cal.day_opening_time = update_opening_time
-					cal.day_closing_time = update_closing_time
-			   	end
-			else
-				if(cal.date == date_of_next(update_day_date))
-				 	cal.day_opening_time = update_opening_time
-				 	cal.day_closing_time = update_closing_time
-				end
-			end
-		end			
-	end
-
-	def closed(day_date)
-		@calendar.each do |cal|
-			if day_date.match('[A-z]{3}\s\d{2}[,]\s\d{4}')
-				 if(cal.date == Date.parse(day_date))
-				 	cal.is_opened = "closed"	
-				 end 
-			else
-				 if(cal.date == date_of_next(day_date))
-				 	cal.is_opened = "closed"
-				 end 
-			end	
+	def closed(*day_date)
+		day_date.each do |item|
+			day = item.is_a?(Symbol) ? item : Date.parse(item) 	
+			@closed_days[day] = true
 		end
 	end
 
 	def calculate_deadline(time_interval, start_date_time)
-
-		#HERE I AM FACING PROBLEM....
-		start_time = start_date_time.split(' ')
-		@calendar.each do |cal|
-			if(cal.date == Date.parse(start_date_time))
-				time_deff_in_seconds = timeDefference(start_time[3] + "" + start_time[4] , cal.day_closing_time)
-				if time_interval > time_deff_in_seconds
-					
-					if cal.is_opened == "opened"
-						@deadline = Time.parse(start_date_time) + time_deff_in_seconds
-						# puts "deadline :"
-						# puts @deadline
-						 time_deff_in_seconds =  time_interval - time_deff_in_seconds
-
-						# puts "time interval"
-						# puts time_interval
-						start_date_time  = (Date.parse(start_date_time) + 1).to_s
-					end
-				else
-					@deadline = Time.parse(start_date_time) + time_interval 
-				end
-
-			end
+		start_time = Time.parse(start_date_time)
+		start_date = Date.parse(start_date_time)
+		if @closed_days[start_date]
+			new_date = start_date.next
+			update_time(start_time,new_date,time_interval)
+		elsif @closed_days[start_time.strftime("%a").downcase.to_sym]
+			new_date = start_date.next
+			update_time(start_time,new_date,time_interval)
+		else
+			update_time(start_time,start_date,time_interval)	
 		end
 	end
+ 	
+ 	def update_time(time,date,time_interval)
+ 		#new_date = date.next
+ 		if @update_days[date]
+			opening_time, closing_time = @update_days[date]
 
-	def date_of_next(day)
-		date  = Date.parse(day)
-		delta = date > @todays_date ? 0 : 7
-		date + delta
-	end
+			time_deff_in_seconds = timeDefference(time.strftime("%R %p") , closing_time)
+			if time_interval > time_deff_in_seconds
+				puts "I AM HERE...."
+				@time_left = time_interval - time_deff_in_seconds
+				calculate_deadline(@time_left, date.to_s + ' ' + opening_time.to_s)
+			else
+				Time.parse(opening_time) + time_interval
+			end	
+		elsif @update_days[time.strftime("%a").downcase.to_sym]
+			opening_time, closing_time = @update_days[time.strftime("%a").downcase.to_sym]
+			time_deff_in_seconds = timeDefference(time.strftime("%R %p") , closing_time)
+			if time_interval > time_deff_in_seconds
+				puts "I AM HERE TOOO...."
+				@time_left = time_interval - time_deff_in_seconds
+				calculate_deadline(@time_left,date.to_s + ' ' + opening_time.to_s)
+			else
+				Time.parse(opening_time) + time_interval
+			end			
+		else
+			opening_time, closing_time = @default_opening_time, @default_closing_time
+			time_deff_in_seconds = timeDefference(time.strftime("%R %p") , closing_time)
+			if time_interval > time_deff_in_seconds
 
+				puts "I AM HERE 420"
+				@time_left = time_interval - time_deff_in_seconds
+				calculate_deadline(@time_left, date.to_s + ' ' + opening_time.to_s)
+			else
+				Time.parse(opening_time) + time_interval
+			end	
+		end	
+
+ 	end
+	
 	def timeDefference(start_time, closing_time)
 		(Time.parse(start_time) - Time.parse(closing_time)).abs.to_i
 	end
 
 	def show
-		@calendar.each do |cal|
-		 	puts cal
-		end	
+	#	pp @update_days
+	puts @deadline
 	end
 
 end
